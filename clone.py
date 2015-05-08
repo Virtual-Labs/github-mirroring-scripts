d8247a252b14a4d43e7fbec865dad8c0b0a4cdc3
root <root@git-backup.vlabs.ac.in>
2015-05-08 10:30:48 +0530

    changed cloning location

--- clone.py
+++ clone.py
@@ -6,6 +6,6 @@ with open('existing_github_repo_list.txt' , 'r') as clone_url_list:
 	for clone_url in clone_url_list:
 		clone_url = clone_url.strip()
 		repo_name = clone_url.split('/')[-1].split('.git')[0]
-		cmd = 'git clone %s %s%s' % (clone_url, 'Virtual-labs/', repo_name)
+		cmd = 'git clone %s %s%s' % (clone_url, '/root/Virtual-labs/', repo_name)
 		print cmd
 		os.system(cmd)
--- clone_or_pull.py
+++ clone_or_pull.py
@@ -5,7 +5,7 @@ with open('latest_github_repo_list.txt', 'r') as current_list:
 	for line in current_list:
 		repo_name = line.split('/')[-1].split('.git')[0]
 		if line in open('existing_github_repo_list.txt').read():
-			cmd = 'git --git-dir=%s%s%s %s' % ('Virtual-labs/', repo_name, '/.git', 'pull')
+			cmd = 'git --git-dir=%s%s%s %s' % ('/root/Virtual-labs/', repo_name, '/.git', 'pull')
 			os.system(cmd)
 		else:
 			cmd = 'git clone %s %s%s' % (line.strip(), 'Virtual-labs/', repo_name)
