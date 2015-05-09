4cb87c3c18c0b3fb3c937cd682c9c85f78ef6523
apurv111 <apurv@vlabs.ac.in>
2015-05-09 11:10:21 +0530

    changes path to pull files

--- backup.py
+++ backup.py
@@ -8,7 +8,7 @@ with open('list_of_repos.txt', 'r') as repos_to_clone:
 	for line in repos_to_clone:
 		line = line.strip()
 		repo_name = line.split('/')[-1].split('.git')[0]
-		if os.path.exists(repo_name) == False:
+		if os.path.exists(os.path.join(path, repo_name)) == False:
 			cmd = 'git clone %s %s%s' % (line, path, repo_name)
 		else:
 			cmd = 'git --git-dir=%s%s%s %s' % (path, repo_name, '/.git', 'pull')
